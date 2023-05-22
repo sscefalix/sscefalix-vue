363c07fae26eb0ee95f713c8d9b872291f7bf2c6
sscefalix <dimabykov189@gmail.com>
2023-05-22 21:12:26 +0300

    Hash history

--- src/router/router.ts
+++ src/router/router.ts
@@ -1,13 +1,13 @@
-import {createRouter, createWebHistory} from "vue-router";
+import {createRouter, createWebHashHistory} from "vue-router";
 import Index from "../components/screens/Index.vue";
 import NoMeta from "../components/screens/NoMeta.vue";
 
 const router = createRouter({
-    history: createWebHistory(),
+    history: createWebHashHistory(),
     routes: [
         {name: "index", path: "/", component: Index},
         {name: "noMeta", path: "/nometa", component: NoMeta}
-    ]
+    ],
 })
 
 export default router;
