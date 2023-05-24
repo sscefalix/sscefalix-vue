9a74d6ebec81456da324b26857270f0d96c033de
sscefalix <100369942+sscefalix@users.noreply.github.com>
2023-05-24 14:55:20 +0300

    Update router.ts

--- src/router/router.ts
+++ src/router/router.ts
@@ -1,9 +1,9 @@
-import {createRouter, createWebHistory} from "vue-router";
+import {createRouter, createWebHashHistory} from "vue-router";
 import Index from "../components/screens/Index.vue";
 import NoMeta from "../components/screens/NoMeta.vue";
 
 const router = createRouter({
-    history: createWebHistory(),
+    history: createWebHashHistory(),
     routes: [
         {name: "index", path: "/", component: Index},
         {name: "noMeta", path: "/nometa", component: NoMeta}
